7952c6e62e8c9cdea61e688ef8319fa78dfa2001
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-07 14:14:47 +0530

    clear the timers using cleanups and use the key property to reset the progress bar

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import quizCompleteImage from '../assets/quiz-complete.png'
 import QUESTIONS from '../questions'
+import QuestionTimer from './QuestionTimer'
 
 export default function Quiz() {
 
@@ -9,11 +10,13 @@ export default function Quiz() {
     const activeQuestionIndex = userAnswers.length
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
     
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setAnswers(prevAns => {
             return [...prevAns, selectedAnswer]
         })
-    }
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if(quizIsComplete){
         return (
@@ -30,6 +33,7 @@ export default function Quiz() {
 
     return <div id="quiz">
         <div id="question">
+            <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}/>
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">
                 { shuffledAnswers.map(answer => (
