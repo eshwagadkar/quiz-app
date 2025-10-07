966e9ade994e9b41f98638d8357663fba0f8919f
eshwagadkar <eshwagadkar007@gmail.com>
2025-10-07 12:16:27 +0530

    Initial commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+import QUESTIONS from "../questions"
+
+export default function Quiz() {
+
+    const [userAnswers, setAnswers] = useState([])
+    
+    const activeQuestionIndex = userAnswers.length
+    
+    function handleSelectAnswer(selectedAnswer) {
+        setAnswers(prevAns => {
+            return [...prevAns, selectedAnswer]
+        })
+    }
+
+    return <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                { QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>
+}
